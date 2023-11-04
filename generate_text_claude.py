336e92603d372a76c98fb8d702760063b40b21f5
Abhishek Gupta <innovative.abhishek@hotmail.com>
2023-11-03 17:49:26 -0700

    First Commit

---
+++ generate_text_claude.py
@@ -0,0 +1,28 @@
+import boto3
+import json
+
+prompt_data = """
+Write a short tweet about welcoming the audience 
+in AWS Community Event Jaipur, in a funny way.
+"""
+
+bedrock = boto3.client(service_name="bedrock-runtime")
+payload = {
+    "prompt": f"\n\nHuman:{prompt_data}\n\nAssistant:",
+    "max_tokens_to_sample": 512,
+    "temperature": 0.8,
+    "top_p": 0.8,
+}
+
+body = json.dumps(payload)
+model_id = "anthropic.claude-v2"
+response = bedrock.invoke_model(
+    body=body,
+    modelId=model_id,
+    accept="application/json",
+    contentType="application/json",
+)
+
+response_body = json.loads(response.get("body").read())
+response_text = response_body.get("completion")
+print(response_text)
